bd44b2a4c5762519e0b19835df71ee178d6e003d
Fukumoto <f1450210@gmail.com>
2024-01-17 23:15:08 +0900

    configファイルの参照先の修正

--- proxy-api/src/utils.py
+++ proxy-api/src/utils.py
@@ -1,14 +1,20 @@
-import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import yaml
 
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 
 def get_config(configFileName: str):
-    with open(os.path.join('config', configFileName), 'r') as yml:
+    # 現在のスクリプトファイル（utils.py）の絶対パスを取得
+    base_path = os.path.dirname(os.path.abspath(__file__))
+
+    # 'config' ディレクトリと configFileName を絶対パスに追加
+    config_path = os.path.join(base_path, '..', 'config', configFileName)
+
+    # 設定ファイルを開いて読み込む
+    with open(config_path, 'r') as yml:
         config = yaml.safe_load(yml)
+
     return config
 
 # シークレットを取得する関数
